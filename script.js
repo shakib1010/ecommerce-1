692f6fc863d346600b47a24a2608433f581eccfc
Shakibul Islam <8080.shakib@gmail.com>
2023-03-02 11:55:32 +0600

    Medium device responsiveness completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Script for navigation bar
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+
+console.log(bar, nav);
+
+bar.addEventListener("click", (e) => {
+  nav.classList.add("active");
+});
+
+close.addEventListener("click", (e) => {
+  nav.classList.remove("active");
+});
